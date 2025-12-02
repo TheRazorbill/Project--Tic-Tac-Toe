ee0356c3197a925380bb26b47801f8cd32322754
rahian <ter26621@gmail.com>
2025-12-02 06:56:43 -0300

    feat: implement win condition logic and integrate game loop

--- script.js
+++ script.js
@@ -25,6 +25,41 @@ const gameController = (() => {
   let round = 0;
   const player1 = player("Player 1", "X");
   const player2 = player("Player 2", "O");
+  const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+function checkWinner() {
+  const board = Gameboard.getBoard(); 
+  
+  for (let i = 0; i < winningConditions.length; i++) {
+    const condition = winningConditions[i];
+    
+    const cellA = board[condition[0]];
+    const cellB = board[condition[1]];
+    const cellC = board[condition[2]];
+
+    if (cellA === "") {
+      continue;
+    }
+
+    if (cellA === cellB && cellB === cellC) {
+      return cellA;
+    }
+  }
+
+  if (!board.includes("")) {
+    return "Empate";
+  }
+  
+  return null;
+}
   const getCurrentPlayer = () => {
     return round % 2 === 0 ? player1 : player2;
   };
@@ -33,6 +68,10 @@ const gameController = (() => {
     const moveSuccessful = Gameboard.setField(index, currentPlayer.marker);
     if (moveSuccessful) {
       round++;
+      const result = checkWinner();
+      if(result !== null){
+        console.log(result)
+      }
       return true;
     }
     return false;
